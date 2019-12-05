357f2922ee2d7a5d4f21d19a836abf1c166f7dac
Admin <admin@Admins-MacBook-Pro.local>
2019-12-04 21:32:42 -0500

    npm i bootstrap react-dates

---
+++ wowo-app/src/components/ClientDashboard/FindWash/ScheduleWash.js
@@ -0,0 +1,23 @@
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-dates/initialize'
+import 'react-dates/lib/css/_datepicker.css';
+import {SingleDatePicker} from 'react-dates';
+
+class Schedule extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <SingleDatePicker
+  date={this.state.date} // momentPropTypes.momentObj or null
+  onDateChange={date => this.setState({ date })} // PropTypes.func.isRequired
+  focused={this.state.focused} // PropTypes.bool
+  onFocusChange={({ focused }) => this.setState({ focused })} // PropTypes.func.isRequired
+  id="your_unique_id" // PropTypes.string.isRequired,
+/>
+
+         );
+    }
+}
+ 
+export default Schedule;
