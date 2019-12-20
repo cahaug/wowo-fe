053b3a6c8aaeebfa7cee045c81e6b26a91d8df1f
malarc01 <45378877+malarc01@users.noreply.github.com>
2019-12-20 12:02:55 -0500

    added .env.production & dev

--- wowo-app/src/actions/actionTypes.js
+++ wowo-app/src/actions/actionTypes.js
@@ -1,62 +1,62 @@
-import axios from "axios";
+import axios from 'axios';
 
 // User Actions
-export const GET_USER = "GET_USER";
-export const GET_USERS = "GET_USERS";
+export const GET_USER = 'GET_USER';
+export const GET_USERS = 'GET_USERS';
 
 // Washer Signup action types
-export const CREATE_WASHER_START = "CREATE_WASHER_START";
-export const CREATE_WASHER_SUCCESS = "CREATE_WASHER_SUCCESS";
-export const CREATE_WASHER_FAILED = "CREATE_WASHER_FAILED";
-export const LOGGING_IN = "LOGGING_IN";
-export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
-export const LOGIN_FAILED = "LOGIN_FAILED";
+export const CREATE_WASHER_START = 'CREATE_WASHER_START';
+export const CREATE_WASHER_SUCCESS = 'CREATE_WASHER_SUCCESS';
+export const CREATE_WASHER_FAILED = 'CREATE_WASHER_FAILED';
+export const LOGGING_IN = 'LOGGING_IN';
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
+export const LOGIN_FAILED = 'LOGIN_FAILED';
 
 // User signup actions
-export const LOADING = "LOADING";
-export const NEW_CLIENT_SUCCESS = "NEW_CLIENT_SUCCESS";
-export const NEW_CLIENT_ERROR = "NEW_CLIENT_ERROR";
+export const LOADING = 'LOADING';
+export const NEW_CLIENT_SUCCESS = 'NEW_CLIENT_SUCCESS';
+export const NEW_CLIENT_ERROR = 'NEW_CLIENT_ERROR';
 
 export function loginUser(email, password) {
-  return dispatch => {
-    dispatch({ type: LOGGING_IN });
+	return (dispatch) => {
+		dispatch({ type: LOGGING_IN });
 
-    return axios
-      .post("https://pt6-wowo.herokuapp.com/auth/login", { email, password })
-      .then(res => {
-        console.log(res.data, "res.data");
-        localStorage.setItem("token", res.data.token);
-        localStorage.setItem("userType", res.data.userType);
-        localStorage.setItem("firstName", res.data.firstName);
-        localStorage.setItem("lastName", res.data.lastName);
-        localStorage.setItem("id", res.data.id);
-        dispatch({
-          type: LOGIN_SUCCESS,
-          payload: res.data
-        });
-      })
-      .catch(err => {
-        dispatch({
-          type: LOGIN_FAILED,
-          payload: err
-        });
-      });
-  };
+		return axios
+			.post('https://boiling-chamber-11659.herokuapp.com/login', { email, password })
+			.then((res) => {
+				console.log(res.data, 'res.data');
+				localStorage.setItem('token', res.data.token);
+				localStorage.setItem('userType', res.data.userType);
+				localStorage.setItem('firstName', res.data.firstName);
+				localStorage.setItem('lastName', res.data.lastName);
+				localStorage.setItem('id', res.data.id);
+				dispatch({
+					type: LOGIN_SUCCESS,
+					payload: res.data
+				});
+			})
+			.catch((err) => {
+				dispatch({
+					type: LOGIN_FAILED,
+					payload: err
+				});
+			});
+	};
 }
 
 export function createClient(payload) {
-  return dispatch => {
-    dispatch({ type: LOADING });
+	return (dispatch) => {
+		dispatch({ type: LOADING });
 
-    return axios
-      .post("https://pt6-wowo.herokuapp.com/auth/registerClient", payload)
-      .then(res => {
-        console.log(res);
-        dispatch({ type: NEW_CLIENT_SUCCESS, payload: res.data });
-      })
-      .catch(err => {
-        console.log(err.response.data);
-        dispatch({ type: NEW_CLIENT_ERROR, payload: err.response.data });
-      });
-  };
+		return axios
+			.post('https://boiling-chamber-11659.herokuapp.com/', payload)
+			.then((res) => {
+				console.log(res);
+				dispatch({ type: NEW_CLIENT_SUCCESS, payload: res.data });
+			})
+			.catch((err) => {
+				console.log(err.response.data);
+				dispatch({ type: NEW_CLIENT_ERROR, payload: err.response.data });
+			});
+	};
 }
